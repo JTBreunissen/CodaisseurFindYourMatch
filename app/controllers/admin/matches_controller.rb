2735f281acb6b2b6da891b1ca626db6b320a7ad7
JTBreunissen <jantjarco@gmail.com>
2017-04-20 14:59:13 +0200

    made 2 homepages based on role

--- app/controllers/admin/matches_controller.rb
+++ app/controllers/admin/matches_controller.rb
@@ -1,6 +1,10 @@
 class Admin::MatchesController < ApplicationController
   before_action :authenticate_user!
 
+  before_action do
+    check_admin
+  end
+
   def index
     matches_index = Match.all
     @matches = matches_index.group_by(&:day)
@@ -15,6 +19,10 @@ class Admin::MatchesController < ApplicationController
   def delete
     Match.removeMatches(params[:day])
     redirect_to admin_matches, notice: "Matches deleted"
+  end
+
+  def check_admin
+    redirect_to "/", alert: 'This page is for Teachers & Staff only.' unless current_user.is_admin
   end
 
 end
