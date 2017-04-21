a64d96dbd61902034893516789a628dd707f97d6
JTBreunissen <jantjarco@gmail.com>
2017-04-21 09:32:20 +0200

    fixed devise sign up to also store student name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  # def after_sign_in_path_for(resource)
-  #   if current_user.is_admin
-  #     redirect_to admin_matches_path, notice: 'Welcome.'
-  #   else
-  #     redirect_to "/"
-  #   end
-  # end
+  def configure_permitted_parameters
+      added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
 end
